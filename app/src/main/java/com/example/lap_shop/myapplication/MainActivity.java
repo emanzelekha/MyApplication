5257c0e4a98588f252d6f779250a1fc177d7a087
emanzelekha <emanzelekha@gmail.com>
2017-03-12 17:51:00 +0200

    yugsrfdfdf

--- app/src/main/java/com/example/lap_shop/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lap_shop/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.lap_shop.myapplication;
 
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,81 +16,88 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.lap_shop.myapplication.Message_home.Home;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     Thread splashTread;
+     int height = 0, width = 0;
     TextView textView;
-    View iv;
     Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Display display = getWindowManager().getDefaultDisplay();
-//        Point size = new Point();
-//        display.getSize(size);
-//        int width = size.x;
-//        int height = size.y;
-//        View square = findViewById(R.id.wight);
-//        textView = (TextView) findViewById(R.id.text);
-//
-//        square.setLayoutParams(new RelativeLayout.LayoutParams(width / 6, width / 6));
-//        textView.setTextSize(width / 70);
-//        StartAnimations();
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+        System.out.println(width+" "+height+"ffffjh");
+        textView = (TextView) findViewById(R.id.text);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) textView.getLayoutParams();
+        params.setMargins(0, height / 6, 0, 0);
+        textView.setLayoutParams(params);
+
+        StartAnimations();
 
 
     }
 
 
     private void StartAnimations() {
-//       anim = AnimationUtils.loadAnimation(this, R.anim.alph);
-//        anim.reset();
-//        RelativeLayout l = (RelativeLayout) findViewById(R.id.lin_lay);
-//        l.clearAnimation();
-//        l.startAnimation(anim);
-//
-//        anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
-//        anim.reset();
-//       // anim.setRepeatCount(Animation.INFINITE);
-//
-//        iv = findViewById(R.id.wight);
-//
-//
-//        iv.clearAnimation();
-//        iv.startAnimation(anim);
 
-//
-//        splashTread = new Thread() {
-//            @Override
-//            public void run() {
-//                try {
-//                    int waited = 0;
-//                    // Splash screen pause time
-//                    while (waited < 3500) {
-//
-//                        sleep(100);
-//                        waited += 100;
-//                    }
-//                 anim.cancel();
-//
-//                    // MainActivity.this.finish();
-//
-//
-//                } catch (InterruptedException e) {
-//                    // do nothing
-//                } finally {
-//                    // MainActivity.this.finish();
-//                    //iv.clearAnimation();
-//                    anim.cancel();
-//                }
-//
-//            }
-//        };
-//        splashTread.start();
+        anim = AnimationUtils.loadAnimation(this, R.anim.alph);
+        anim.reset();
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setRepeatMode(Animation.REVERSE);
+
+        textView.clearAnimation();
+        textView.startAnimation(anim);
+
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+
+                        sleep(100);
+                        waited += 100;
+                    }
+                    anim.cancel();
+                    Intent intent = new Intent(MainActivity.this,
+                            Home.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    MainActivity.this.finish();
+
+
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    MainActivity.this.finish();
+                    //iv.clearAnimation();
+                    anim.cancel();
+                }
+
+            }
+        };
+        splashTread.start();
 
     }
+//    public String getHeght(){
+//        Display display = getWindowManager().getDefaultDisplay();
+//        Point size = new Point();
+//        display.getSize(size);
+//        width = size.x;
+//        height = size.y;
+//        return  width+" "+height;
+//    }
 
 }
